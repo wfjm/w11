wfjm/w11

commit 970be66d07805e8a62ef2e9fd8bf1eaa40d5ccef

cleanups for -Wpedantic; bugfix in RtclArgs.hpp

tools/src/librw11/Rw11CntlDEUNA.cpp
@@ -1,4 +1,4 @@
-// $Id: Rw11CntlDEUNA.cpp 1048 2018-09-22 07:41:46Z mueller $
+// $Id: Rw11CntlDEUNA.cpp 1049 2018-09-22 13:56:52Z mueller $
 //
 // Copyright 2014-2018 by Walter F.J. Mueller <W.F.J.Mueller@gsi.de>
 //
@@ -350,7 +350,7 @@ void Rw11CntlDEUNA::Start()
 //------------------------------------------+-----------------------------------
 //! FIXME_docs
 
-void Rw11CntlDEUNA::UnitSetup(size_t ind)
+void Rw11CntlDEUNA::UnitSetup(size_t /*ind*/)
 {
   RlinkCommandList clist;
   Rw11Cpu& cpu  = Cpu();
@@ -1365,10 +1365,10 @@ void Rw11CntlDEUNA::StartTxRing()
   RlinkCommandList clist;
   uint16_t txdsccur[4];
   uint16_t txdscnxt[4];
-  size_t irdtxdsccur = Cpu().AddRMem(clist, TxRingDscAddr(fTxRingIndex),
-                                     txdsccur, 3, Rw11Cpu::kCPAH_M_UBM22, true);
-  size_t irdtxdscnxt = Cpu().AddRMem(clist, TxRingDscAddr(TxRingIndexNext()),
-                                     txdscnxt, 3, Rw11Cpu::kCPAH_M_UBM22, true);
+  //size_t irdtxdsccur = Cpu().AddRMem(clist, TxRingDscAddr(fTxRingIndex),
+  //                                 txdsccur, 3, Rw11Cpu::kCPAH_M_UBM22, true);
+  //size_t irdtxdscnxt = Cpu().AddRMem(clist, TxRingDscAddr(TxRingIndexNext()),
+  //                                 txdscnxt, 3, Rw11Cpu::kCPAH_M_UBM22, true);
   Server().Exec(clist);
   // FIXME_code: check dsc read errors !  
   StartTxRing(txdsccur, txdscnxt);
@@ -1408,10 +1408,10 @@ void Rw11CntlDEUNA::StartRxRing()
   RlinkCommandList clist;
   uint16_t rxdsccur[4];
   uint16_t rxdscnxt[4];
-  size_t irdrxdsccur = Cpu().AddRMem(clist, RxRingDscAddr(fRxRingIndex),
-                                     rxdsccur, 3, Rw11Cpu::kCPAH_M_UBM22, true);
-  size_t irdrxdscnxt = Cpu().AddRMem(clist, RxRingDscAddr(RxRingIndexNext()),
-                                     rxdscnxt, 3, Rw11Cpu::kCPAH_M_UBM22, true);
+  //size_t irdrxdsccur = Cpu().AddRMem(clist, RxRingDscAddr(fRxRingIndex),
+  //                                 rxdsccur, 3, Rw11Cpu::kCPAH_M_UBM22, true);
+  //size_t irdrxdscnxt = Cpu().AddRMem(clist, RxRingDscAddr(RxRingIndexNext()),
+  //                                rxdscnxt, 3, Rw11Cpu::kCPAH_M_UBM22, true);
   Server().Exec(clist);
   // FIXME_code: check dsc read errors !  
   StartRxRing(rxdsccur, rxdscnxt);

tools/src/librlink/ReventLoop.cpp
@@ -1,6 +1,6 @@
-// $Id: ReventLoop.cpp 983 2018-01-02 20:35:59Z mueller $
+// $Id: ReventLoop.cpp 1049 2018-09-22 13:56:52Z mueller $
 //
-// Copyright 2013-2017 by Walter F.J. Mueller <W.F.J.Mueller@gsi.de>
+// Copyright 2013-2018 by Walter F.J. Mueller <W.F.J.Mueller@gsi.de>
 //
 // This program is free software; you may redistribute and/or modify it under
 // the terms of the GNU General Public License as published by the Free
@@ -185,7 +185,7 @@ void ReventLoop::EventLoop()
 //! FIXME_docs
 
 void ReventLoop::Dump(std::ostream& os, int ind, const char* text,
-                      int detail) const
+                      int /*detail*/) const
 {
   RosFill bl(ind);
   os << bl << (text?text:"--") << "ReventLoop @ " << this << endl;

tools/src/librlink/RlinkChannel.cpp
@@ -1,6 +1,6 @@
-// $Id: RlinkChannel.cpp 983 2018-01-02 20:35:59Z mueller $
+// $Id: RlinkChannel.cpp 1049 2018-09-22 13:56:52Z mueller $
 //
-// Copyright 2013-2017 by Walter F.J. Mueller <W.F.J.Mueller@gsi.de>
+// Copyright 2013-2018 by Walter F.J. Mueller <W.F.J.Mueller@gsi.de>
 //
 // This program is free software; you may redistribute and/or modify it under
 // the terms of the GNU General Public License as published by the Free
@@ -68,7 +68,7 @@ bool RlinkChannel::Exec(RlinkCommandList& clist, RerrMsg& emsg)
 //! FIXME_docs
 
 void RlinkChannel::Dump(std::ostream& os, int ind, const char* text,
-                        int detail) const
+                        int /*detail*/) const
 {
   RosFill bl(ind);
   os << bl << (text?text:"--") << "RlinkChannel @ " << this << endl;

tools/src/librlink/RlinkCommandExpect.cpp
@@ -1,6 +1,6 @@
-// $Id: RlinkCommandExpect.cpp 983 2018-01-02 20:35:59Z mueller $
+// $Id: RlinkCommandExpect.cpp 1049 2018-09-22 13:56:52Z mueller $
 //
-// Copyright 2011-2017 by Walter F.J. Mueller <W.F.J.Mueller@gsi.de>
+// Copyright 2011-2018 by Walter F.J. Mueller <W.F.J.Mueller@gsi.de>
 //
 // This program is free software; you may redistribute and/or modify it under
 // the terms of the GNU General Public License as published by the Free
@@ -134,7 +134,7 @@ bool RlinkCommandExpect::BlockIsChecked(size_t ind) const
 //! FIXME_docs
 
 void RlinkCommandExpect::Dump(std::ostream& os, int ind, const char* text,
-                              int detail) const
+                              int /*detail*/) const
 {
   RosFill bl(ind);
   os << bl << (text?text:"--") << "RlinkCommandExpect @ " << this << endl;

tools/src/librlink/RlinkContext.cpp
@@ -1,6 +1,6 @@
-// $Id: RlinkContext.cpp 983 2018-01-02 20:35:59Z mueller $
+// $Id: RlinkContext.cpp 1049 2018-09-22 13:56:52Z mueller $
 //
-// Copyright 2013-2017 by Walter F.J. Mueller <W.F.J.Mueller@gsi.de>
+// Copyright 2013-2018 by Walter F.J. Mueller <W.F.J.Mueller@gsi.de>
 //
 // This program is free software; you may redistribute and/or modify it under
 // the terms of the GNU General Public License as published by the Free
@@ -57,7 +57,7 @@ RlinkContext::~RlinkContext()
 //! FIXME_docs
 
 void RlinkContext::Dump(std::ostream& os, int ind, const char* text,
-                        int detail) const
+                        int /*detail*/) const
 {
   RosFill bl(ind);
   os << bl << (text?text:"--") << "RlinkContext @ " << this << endl;

tools/src/librlinktpp/RtclAttnShuttle.cpp
@@ -1,6 +1,6 @@
-// $Id: RtclAttnShuttle.cpp 983 2018-01-02 20:35:59Z mueller $
+// $Id: RtclAttnShuttle.cpp 1049 2018-09-22 13:56:52Z mueller $
 //
-// Copyright 2013-2014 by Walter F.J. Mueller <W.F.J.Mueller@gsi.de>
+// Copyright 2013-2018 by Walter F.J. Mueller <W.F.J.Mueller@gsi.de>
 //
 // This program is free software; you may redistribute and/or modify it under
 // the terms of the GNU General Public License as published by the Free
@@ -142,7 +142,7 @@ int RtclAttnShuttle::AttnHandler(RlinkServer::AttnArgs& args)
 //------------------------------------------+-----------------------------------
 //! FIXME_docs
 
-void RtclAttnShuttle::TclChannelHandler(int mask)
+void RtclAttnShuttle::TclChannelHandler(int /*mask*/)
 {
   uint16_t apat;
   Tcl_ReadRaw(fShuttleChn, (char*) &apat, sizeof(apat));

tools/src/librtcltools/Rtcl.hpp
@@ -1,6 +1,6 @@
-// $Id: Rtcl.hpp 983 2018-01-02 20:35:59Z mueller $
+// $Id: Rtcl.hpp 1049 2018-09-22 13:56:52Z mueller $
 //
-// Copyright 2011-2013 by Walter F.J. Mueller <W.F.J.Mueller@gsi.de>
+// Copyright 2011-2018 by Walter F.J. Mueller <W.F.J.Mueller@gsi.de>
 //
 // This program is free software; you may redistribute and/or modify it under
 // the terms of the GNU General Public License as published by the Free
@@ -56,7 +56,7 @@ namespace Retro {
 
     void            SetResult(Tcl_Interp* interp, const std::string& str);
     void            SetResult(Tcl_Interp* interp, std::ostringstream& sos);
-  };
+  }
   
 } // end namespace Retro
 

tools/src/librtcltools/RtclArgs.hpp
@@ -1,6 +1,6 @@
-// $Id: RtclArgs.hpp 983 2018-01-02 20:35:59Z mueller $
+// $Id: RtclArgs.hpp 1049 2018-09-22 13:56:52Z mueller $
 //
-// Copyright 2011-2013 by Walter F.J. Mueller <W.F.J.Mueller@gsi.de>
+// Copyright 2011-2018 by Walter F.J. Mueller <W.F.J.Mueller@gsi.de>
 //
 // This program is free software; you may redistribute and/or modify it under
 // the terms of the GNU General Public License as published by the Free
@@ -13,6 +13,7 @@
 // 
 // Revision History: 
 // Date         Rev Version  Comment
+// 2018-09-22  1049   1.0.10 BUGFIX: get *_min limits correct
 // 2013-05-19   521   1.0.9  add NextSubOpt() method, pass optset's as const
 // 2013-03-05   495   1.0.8  add SetResult(bool)
 // 2013-03-02   494   1.0.7  add Quit() method
@@ -50,15 +51,15 @@ namespace Retro {
   class RtclArgs {
     public:
 
-    const static int8_t   int8_min   = 0xff;
-    const static int8_t   int8_max   = 0x7f;
-    const static uint8_t  uint8_max  = 0xff;
-    const static int16_t  int16_min  = 0xffff;
-    const static int16_t  int16_max  = 0x7fff;
-    const static uint16_t uint16_max = 0xffff;
-    const static int32_t  int32_min  = 0xffffffff;
-    const static int32_t  int32_max  = 0x7fffffff;
-    const static uint32_t uint32_max = 0xffffffff;
+    const static int8_t   int8_min   = std::numeric_limits<int8_t>::min();
+    const static int8_t   int8_max   = std::numeric_limits<int8_t>::max();
+    const static uint8_t  uint8_max  = std::numeric_limits<uint8_t>::max();
+    const static int16_t  int16_min  = std::numeric_limits<int16_t>::min();
+    const static int16_t  int16_max  = std::numeric_limits<int16_t>::max();
+    const static uint16_t uint16_max = std::numeric_limits<uint16_t>::max();
+    const static int32_t  int32_min  = std::numeric_limits<int32_t>::min();
+    const static int32_t  int32_max  = std::numeric_limits<int32_t>::max();
+    const static uint32_t uint32_max = std::numeric_limits<uint32_t>::max();
 
                         RtclArgs();
                         RtclArgs(Tcl_Interp* interp, int objc, 

commit 03d69de26452f72d84176176659c683c5402c65e

BUGFIX: Start(Tx|Rx)Ring, was broken in 970be66

tools/src/librw11/Rw11CntlDEUNA.cpp
@@ -1,4 +1,4 @@
-// $Id: Rw11CntlDEUNA.cpp 1075 2018-12-01 11:55:07Z mueller $
+// $Id: Rw11CntlDEUNA.cpp 1078 2018-12-08 14:19:03Z mueller $
 //
 // Copyright 2014-2018 by Walter F.J. Mueller <W.F.J.Mueller@gsi.de>
 //
@@ -13,6 +13,8 @@
 // 
 // Revision History: 
 // Date         Rev Version  Comment
+// 2018-12-08  1078   0.5.3  BUGFIX: Start(Tx|Rx)Ring, was broken in r1049
+//                             when fixing -Wunused-variable warnings
 // 2018-11-30  1075   0.5.2  use list-init
 // 2018-09-22  1048   0.5.1  BUGFIX: coverity (resource leak)
 // 2017-04-17   880   0.5    Initial version (minimal functions, 211bsd ready)
@@ -1375,10 +1377,10 @@ void Rw11CntlDEUNA::StartTxRing()
   RlinkCommandList clist;
   uint16_t txdsccur[4];
   uint16_t txdscnxt[4];
-  //size_t irdtxdsccur = Cpu().AddRMem(clist, TxRingDscAddr(fTxRingIndex),
-  //                                 txdsccur, 3, Rw11Cpu::kCPAH_M_UBM22, true);
-  //size_t irdtxdscnxt = Cpu().AddRMem(clist, TxRingDscAddr(TxRingIndexNext()),
-  //                                 txdscnxt, 3, Rw11Cpu::kCPAH_M_UBM22, true);
+  Cpu().AddRMem(clist, TxRingDscAddr(fTxRingIndex),
+                txdsccur, 3, Rw11Cpu::kCPAH_M_UBM22, true);
+  Cpu().AddRMem(clist, TxRingDscAddr(TxRingIndexNext()),
+                txdscnxt, 3, Rw11Cpu::kCPAH_M_UBM22, true);
   Server().Exec(clist);
   // FIXME_code: check dsc read errors !  
   StartTxRing(txdsccur, txdscnxt);
@@ -1418,10 +1420,10 @@ void Rw11CntlDEUNA::StartRxRing()
   RlinkCommandList clist;
   uint16_t rxdsccur[4];
   uint16_t rxdscnxt[4];
-  //size_t irdrxdsccur = Cpu().AddRMem(clist, RxRingDscAddr(fRxRingIndex),
-  //                                 rxdsccur, 3, Rw11Cpu::kCPAH_M_UBM22, true);
-  //size_t irdrxdscnxt = Cpu().AddRMem(clist, RxRingDscAddr(RxRingIndexNext()),
-  //                                rxdscnxt, 3, Rw11Cpu::kCPAH_M_UBM22, true);
+  Cpu().AddRMem(clist, RxRingDscAddr(fRxRingIndex),
+                rxdsccur, 3, Rw11Cpu::kCPAH_M_UBM22, true);
+  Cpu().AddRMem(clist, RxRingDscAddr(RxRingIndexNext()),
+                rxdscnxt, 3, Rw11Cpu::kCPAH_M_UBM22, true);
   Server().Exec(clist);
   // FIXME_code: check dsc read errors !  
   StartRxRing(rxdsccur, rxdscnxt);
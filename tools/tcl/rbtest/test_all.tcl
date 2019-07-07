b6e47e02376d5beb51ed9830a4feaed13ca09fdd
wfjm <w.f.j.mueller@gsi.de>
2019-07-07 17:52:04 +0200

    SPDX: tools/tcl/*/*.tcl

--- tools/tcl/rbtest/test_all.tcl
+++ tools/tcl/rbtest/test_all.tcl
@@ -1,15 +1,6 @@
-# $Id: test_all.tcl 985 2018-01-03 08:59:40Z mueller $
-#
+# $Id: test_all.tcl 1177 2019-06-30 12:34:07Z mueller $
+# SPDX-License-Identifier: GPL-3.0-or-later
 # Copyright 2011-2016 by Walter F.J. Mueller <W.F.J.Mueller@gsi.de>
-#
-# This program is free software; you may redistribute and/or modify it under
-# the terms of the GNU General Public License as published by the Free
-# Software Foundation, either version 3, or (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful, but
-# WITHOUT ANY WARRANTY, without even the implied warranty of MERCHANTABILITY
-# or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
-# for complete details.
 #
 #  Revision History:
 # Date         Rev Version  Comment
--- tools/tcl/tst_sram/test_regs.tcl
+++ tools/tcl/tst_sram/test_regs.tcl
@@ -1,15 +1,6 @@
-# $Id: test_regs.tcl 1074 2018-11-25 21:38:59Z mueller $
-#
+# $Id: test_regs.tcl 1177 2019-06-30 12:34:07Z mueller $
+# SPDX-License-Identifier: GPL-3.0-or-later
 # Copyright 2016-2018 by Walter F.J. Mueller <W.F.J.Mueller@gsi.de>
-#
-# This program is free software; you may redistribute and/or modify it under
-# the terms of the GNU General Public License as published by the Free
-# Software Foundation, either version 3, or (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful, but
-# WITHOUT ANY WARRANTY, without even the implied warranty of MERCHANTABILITY
-# or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
-# for complete details.
 #
 #  Revision History:
 # Date         Rev Version  Comment
